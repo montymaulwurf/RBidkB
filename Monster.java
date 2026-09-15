244f0809d406233485b240397e310bd690ea4c4e
Monty <pelster.nico@gmail.com>
2026-09-15 15:14:16 +0200

    add Spielklasse & Kampfregel / Held changes

--- Monster.java
+++ Monster.java
@@ -1,7 +1,7 @@
 
 
-public class Monster extends Kreatur
-{
+public class Monster extends Kreatur{
+    
     public Monster(double pAngriffswert, int pLebenspunkte){
         angriffswert = pAngriffswert;
         lebenspunkte = pLebenspunkte;
--- Waffe.java
+++ Waffe.java
@@ -4,20 +4,35 @@ public class Waffe
     private double bonus;
     private String material;
     private double magie;
+    
     public Waffe(String pMaterial, double pMagie){
         material = pMaterial;
         magie = pMagie;
+        bonusBerechnen();
     }
     
     private double materialWert(String pMaterial) {
         int x = 5;
-        if(pMaterial.equals("Holz")){
+
+        if("Stein".equals(pMaterial)){
             return x;
         }
-        else if
+        else if("Bronze".equals(pMaterial)){
+            return x*2;
+        }
+        else if("Eisen".equals(pMaterial)){
+            return x*3;
+        }
+        else{
+            return 0;
+        }
     }
     
-    private void bonusBerechnen(String material, double magie){
-        
+    private void bonusBerechnen(){
+        bonus = materialWert(material) + magie;       
+    }
+
+    public double getBonus(){
+        return bonus;
     }
 }
