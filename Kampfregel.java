3971de0bf6c0cc79a5c73a53ab12f1d169151507
montymaulwurf <pelster.nico@gmail.com>
2026-09-17 13:40:46 +0200

    kampfregel change

--- Kampfregel.java
+++ Kampfregel.java
@@ -1,28 +1,37 @@
 public class Kampfregel{
     
-    private Wuerfel wuerfel6 = new Wuerfel();
-    private Wuerfel wuerfel10 = new Wuerfel(10);
+    private Wuerfel wuerfel1;
+    private Wuerfel wuerfel2;
 
     private boolean result; // result == true bedeutet Held Sieg
     private double heldAngriff; 
     private double monsterAngriff;
 
-    public Kampfregel(Held held, Monster monster){
+    public Kampfregel (int pWuerfelAugenzahl1, int pWuerfelAugenzahl2){
+        wuerfel1 = new Wuerfel(pWuerfelAugenzahl1);
+        wuerfel2 = new Wuerfel(pWuerfelAugenzahl2);
+    }
+
+    public void kampf(Held held, Monster monster){ //this for held
         
         heldAngriff = 0;
         monsterAngriff = 0;
 
-        wuerfel6.werfen();
-        wuerfel10.werfen();
+        wuerfel1.werfen();
+        wuerfel2.werfen();
 
-        heldAngriff = held.getHeldAngriffswert() * wuerfel10.gibAugenzahl() + wuerfel6.gibAugenzahl();
+        heldAngriff = held.getHeldAngriffswert() * wuerfel1.gibAugenzahl() + wuerfel2.gibAugenzahl();
 
-        wuerfel6.werfen();
-        wuerfel10.werfen();
+        wuerfel1.werfen();
+        wuerfel2.werfen();
 
-        monsterAngriff = monster.getMonsterAngriffswert() * wuerfel10.gibAugenzahl() + wuerfel6.gibAugenzahl();
+        monsterAngriff = monster.getMonsterAngriffswert() * wuerfel1.gibAugenzahl() + wuerfel2.gibAugenzahl();
 
-        result = heldAngriff > monsterAngriff; //49.17% winchance bei gleichem Angriffswert
+        result = heldAngriff > monsterAngriff; 
 
     } 
+
+    public boolean getResult(){
+        return result;
+    }
 }
